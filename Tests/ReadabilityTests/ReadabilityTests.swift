0282001ffdec37c69be72005f6787e8b831f222d
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2025-02-06 20:28:38 +0900

    Remove tests

--- Tests/ReadabilityTests/ReadabilityTests.swift
+++ Tests/ReadabilityTests/ReadabilityTests.swift
@@ -1,10 +1,1 @@
-import Testing
-@testable import Readability
-import Foundation
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    let html = try await HTMLFetcher().fetch(url: URL(string: "https://qiita.com/Ryu0118/items/851a96eb1d362ecce11f")!)
-    let result = try await ReadabilityRunner().parse(html: html, options: nil)
-    print(result.textContent)
-}
